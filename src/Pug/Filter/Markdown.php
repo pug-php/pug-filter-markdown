cbbbb9703c3feabcb219a19d3a86ede9c6fd44df
kylekatarnls <kylekatarnls@gmail.com>
2017-06-01 16:20:34 +0200

    AbstractFilter disambiguation

--- src/Pug/Filter/Markdown.php
+++ src/Pug/Filter/Markdown.php
@@ -4,10 +4,10 @@ namespace Pug\Filter;
 
 use cebe\markdown\Markdown as Parser;
 use Jade\Compiler;
-use Jade\Filter\AbstractFilter;
+use Jade\Filter\AbstractFilter as JadeAbstractFilter;
 use Jade\Nodes\Filter;
 
-class Markdown extends AbstractFilter
+class Markdown extends JadeAbstractFilter
 {
     public function __invoke(Filter $node, Compiler $compiler)
     {
